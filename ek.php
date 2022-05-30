25ccfa705c446bd2f3a1bc1b19cc3578e97e923b
Evan Klein <me@evanklein.com>
2022-05-30 09:50:48 -0700

    Progress commit; squash later

--- ek.php
+++ ek.php
@@ -1,1 +1,23 @@
-// TODO
+<?php
+
+namespace ek;
+
+// This function generates a random string
+function randStr($len=16, $chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'){
+	$chars_len = \strlen($chars);
+
+	$rand_str = '';
+
+	while($len>0){
+		$rand_str.=\substr(
+			$chars,
+			\random_int(0, $chars_len-1),
+			1
+		);
+		$len--;
+	}
+
+	return $rand_str;
+}
+
+?>
