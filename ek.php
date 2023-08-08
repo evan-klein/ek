92880b61fa3a6210f6aaf760c84355c9abcf036f
Evan Klein <me@evanklein.com>
2023-08-08 12:38:35 -0700

    Progress commit; squash later

--- ek.php
+++ ek.php
@@ -67,6 +67,14 @@ function randStr($len=16, $chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst
 }
 
 
+/*
+
+  _       __  __        _   _   _   __
+ |_) /\  (_  (_ \    / / \ |_) | \ (_
+ |  /--\ __) __) \/\/  \_/ | \ |_/ __)
+
+*/
+
 // This function generates a random string, like randStr(), except that it excludes characters that look alike (i.e., I, i, L, l, 1, O, o, 0)
 function randPw($len=16): string {
 	return randStr(
